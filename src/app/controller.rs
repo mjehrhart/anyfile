9e3db064a2f0ab8d94590a650e9fffde1f7a76d4
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-21 12:42:21 -0700

    sp

--- src/app/controller.rs
+++ src/app/controller.rs
@@ -1,48 +1,84 @@
+use byte_unit::Byte;
 use eframe::{
     egui,
     epi::{self, Storage},
 };
 use egui::{Color32, ScrollArea, Sense, TextStyle};
 use egui_extras::RetainedImage;
+//use rayon::vec;
 
+use crate::{enums::enums::FileType, file::meta::Meta, return_dfer2};
 use std::path::PathBuf;
 
-pub struct Application {}
-
-impl Application {
+pub struct Application<'a> {
+    pub shrink: bool,
+    pub test_size2: f32,
+    pub test_offset: egui::Vec2,
+    pub test_size: egui::Vec2,
+    pub staging: Vec<(String, Vec<Meta>)>,
+    pub staging2: Vec<(&'a String, &'a Vec<Meta>)>,
+    pub directory: String,
+    pub image_directory: RetainedImage,
+    pub image_exit: RetainedImage,
+    pub image_settings: RetainedImage,
+}
+//&home::home_dir().unwrap()
+impl Application<'_> {
     pub fn default() -> Self {
-        Self {}
+        Self {
+            //
+            staging: vec![],
+            staging2: vec![],
+            directory: String::new(),
+            image_directory: RetainedImage::from_image_bytes(
+                "Directory",
+                include_bytes!("../../resources/folder.png"),
+            )
+            .unwrap(),
+            image_exit: RetainedImage::from_svg_bytes(
+                "Exit",
+                include_bytes!("../../resources/close.svg"),
+            )
+            .unwrap(),
+            image_settings: RetainedImage::from_svg_bytes(
+                "Settingd",
+                include_bytes!("../../resources/settings.svg"),
+            )
+            .unwrap(),
+            //
+            shrink: false,
+            test_size2: 0.,
+            test_offset: egui::Vec2::new(0., 0.),
+            test_size: egui::Vec2::new(35., 35.),
+            // sub: vec![],
+        }
+    }
+
+    pub fn add_label(&mut self, ui: &mut egui::Ui, text: String) {
+        ui.add(egui::Label::new(
+            egui::RichText::new(text).color(egui::Color32::from_rgb(255, 255, 255)),
+        ));
     }
 }
- 
-impl epi::App for Application {
+
+impl epi::App for Application<'_> {
     fn name(&self) -> &str {
-        "Sandbox 0.0.1"
+        "AnyFile 0.0.1"
     }
 
     fn setup(&mut self, _ctx: &egui::Context, _frame: &epi::Frame, _storage: Option<&dyn Storage>) {
+        self.directory = home::home_dir().unwrap().as_path().display().to_string();
+
+        // Testing
+        self.directory = "/Users/matthew/tmp/".to_string();
     }
 
     fn update(&mut self, ctx: &egui::Context, _frame: &epi::Frame) {
         //
         //********************************************************************************************* */
-        // if !ctx.input().raw.hovered_files.is_empty() {
-        //     //
-        //     let mut text = "Dropping files:\n".to_string();
-        //     //
-        //     for file in &ctx.input().raw.hovered_files {
-        //         //
-        //         if let Some(path) = &file.path {
-        //             text += &format!("\n{}", path.display());
-        //         } else if !file.mime.is_empty() {
-        //             text += &format!("\n{}", file.mime);
-        //         } else {
-        //         }
-        //     }
-        // }
-
+        // File Drops
         if !ctx.input().raw.dropped_files.is_empty() {
-            println!("Files dropped");
+            //println!("Files dropped");
 
             let dropped_files = ctx
                 .input()
@@ -53,10 +89,61 @@ impl epi::App for Application {
                 .map(|file| file.path.as_ref().unwrap().clone())
                 .collect::<Vec<PathBuf>>();
 
-            //dbg!(&dropped_files);
-            println!("{:?}", dropped_files);
+            //println!("{:?}", dropped_files);
+
+            //****************************************************** */
+            let mut needles = vec![];
+            for dropped_file in dropped_files {
+                //
+                //let sd = "/Users/matthew/tmp/file_types/audio/Short.mp3";
+                let sd = &dropped_file.as_os_str().to_string_lossy();
+                let filter = [true, true, true, true, true];
+                let single_file = return_dfer2(&sd, filter);
+                needles.push(single_file.data_set);
+            }
+            // Clear self.staging
+            self.staging.clear();
+
+            //println!("needles => {:#?}", needles);
+
+            // Search through Haystack
+            // let sd = "/Users/matthew/tmp/file_types/audio";
+            let filter = [true, true, true, true, true];
+            let multi_batch = return_dfer2(&self.directory, filter);
+            //println!("x => {:#?}", multi_batch);
+            let multi = multi_batch.data_set;
+
+            // Empty vec to store matches
+            let mut matched = vec![];
+            // Loop through files to find matches
+            for i in 0..needles.len() {
+                for (key, value) in &needles[i] {
+                    // Loop through multi
+                    for item in multi.clone() {
+                        //println!("{:#?}", &item.1.l);
+                        if (key == &item.0)
+                        //&& (value[0].path != item.1[0].path)
+                        {
+                            matched.push(item);
+                        }
+                    }
+                }
+            }
+
+            self.staging = matched.clone();
+
+            if self.staging.len() == 0 {
+                let tmp = egui::Vec2::new(260.0, 260.0);
+                _frame.set_window_size(tmp)
+            } else {
+                let tmp = egui::Vec2::new(860., 260.);
+                _frame.set_window_size(tmp);
+            }
+            //dbg!("{:#?}", &self.staging);
         }
 
+        //********************************************************************************************* */
+        // UI
         //********************************************************************************************* */
         let frame_style_1 = egui::containers::Frame {
             margin: egui::style::Margin {
@@ -75,18 +162,169 @@ impl epi::App for Application {
                 extrusion: 0.0,
                 color: Color32::TRANSPARENT,
             },
-            fill: Color32::from_rgb(49, 90, 125),
+            //fill: Color32::from_rgb(49, 90, 125),
+            fill: Color32::TRANSPARENT,
             stroke: egui::Stroke::new(0.0, Color32::from_rgb(255, 255, 255)),
         };
+
+        self::Application::add_top_bar(self, ctx, _frame);
 
         egui::CentralPanel::default()
             .frame(frame_style_1)
             .show(ctx, |ui| {
-                ui.label("Drop Target");
-
-                ui.add( toggle2(&mut true));
-                
+                // Add bullseye
+                ui.add(toggle2(&mut true));
             });
+
+        //ui.add_sized(ui.available_size(), Image::new(…));
+
+        // Right PANEL
+        //fn get_table_fields(dt: &Meta) -> (String, String, String) {
+        fn get_table_fields(dt: &Meta) -> String {
+            let byte = Byte::from_bytes(dt.file_size.try_into().unwrap());
+            let adjusted_byte = byte.get_appropriate_unit(false);
+
+            let mut title: String = String::from("");
+            let mut icon: String = String::from("");
+
+            match dt.file_type {
+                FileType::Image => {
+                    title = format!("{}", dt.path);
+                    icon = "🖼".to_string();
+                }
+                FileType::Audio => {
+                    title = format!("{}", dt.path);
+                    icon = "🎵".to_string();
+                }
+                FileType::Video => {
+                    title = format!("{}", dt.path);
+                    icon = "🎞".to_string();
+                }
+                FileType::Document => {
+                    title = format!("{}", dt.path);
+                    icon = "📎".to_string();
+                }
+                FileType::Other => {
+                    title = format!("{}", dt.path);
+                    icon = "📝".to_string();
+                }
+                FileType::None => {}
+                FileType::All => {}
+            }
+
+            // title
+            title = title.to_lowercase();
+            if title.len() < 60 {
+                title = title[0..title.len()].to_string();
+            } else {
+                title = title[0..60].to_string();
+            }
+            let diff = 60 - title.to_string().chars().count();
+            let mut space: String = String::from("");
+            for _ in 0..diff {
+                space.push(' ');
+            }
+            title = [title.to_string(), space].join("");
+
+            // adjusted_byte
+            let diff = 10 - adjusted_byte.to_string().chars().count();
+            let mut space: String = String::from("");
+            for _ in 0..diff {
+                space.push(' ');
+            }
+            let adjusted_byte = [space, adjusted_byte.to_string()].join("");
+
+            let full_title = [icon, title, adjusted_byte].join("  ");
+            //(title, adjusted_byte, icon)
+            full_title
+        }
+
+        egui::SidePanel::right("right_panel")
+            .frame(frame_style_1)
+            //.min_width(self.test_size2)
+            .min_width(630.)
+            .resizable(true)
+            .show(ctx, |ui| {
+                ScrollArea::vertical()
+                    .id_source("bottom_scroll2")
+                    .auto_shrink([false, false])
+                    .max_height(220.)
+                    .min_scrolled_height(220.)
+                    .stick_to_right()
+                    .show(ui, |ui| {
+                        //
+                        ui.vertical(|ui| {
+                            for row in &self.staging {
+                                // adjusted_byte
+                                //let diff = 100 - &row.1[0].name.to_string().chars().count();
+                                // title
+                                let mut title = row.1[0].name.to_lowercase();
+                                if title.len() < 100 {
+                                    title = title[0..title.len()].to_string();
+                                } else {
+                                    title = title[0..100].to_string();
+                                }
+
+                                let diff = 100 - title.to_string().chars().count();
+                                let mut space: String = String::from("");
+                                for _ in 0..diff {
+                                    space.push(' ');
+                                }
+                                title = [title.to_string(), space].join("");
+
+                                ui.with_layout(egui::Layout::top_down(egui::Align::LEFT), |ui| {
+                                    ui.add(egui::Label::new(
+                                        egui::RichText::new(title)
+                                            .color(egui::Color32::from_rgb(255, 255, 255))
+                                            .monospace(),
+                                    ));
+                                });
+                                egui::Grid::new("grid_main_labels")
+                                    .striped(true)
+                                    .num_columns(2)
+                                    .striped(true)
+                                    .spacing(egui::Vec2::new(10.0, 0.0))
+                                    .show(ui, |ui| {
+                                        ui.with_layout(
+                                            egui::Layout::top_down(egui::Align::RIGHT),
+                                            |ui| {
+                                                for item in &row.1 {
+                                                    let full_title = get_table_fields(item);
+
+                                                    ui.add_sized(
+                                                        [600., 10.0],
+                                                        egui::Button::new(
+                                                            egui::RichText::new(full_title)
+                                                                .color(egui::Color32::from_rgb(
+                                                                    255, 255, 255,
+                                                                ))
+                                                                .monospace(),
+                                                        )
+                                                        //.fill(egui::Color32::from_rgb(49, 90, 125)),
+                                                        .fill(egui::Color32::TRANSPARENT),
+                                                    );
+                                                    ui.add_sized(
+                                                        [100.0, 10.0],
+                                                        egui::Hyperlink::from_label_and_url(
+                                                            "VIEW", &item.path,
+                                                        ),
+                                                    );
+                                                }
+                                            },
+                                        );
+                                    });
+                            }
+                        });
+                    });
+
+                // let widget_rect =
+                //     egui::Rect::from_min_size(ui.min_rect().min + self.test_offset, self.test_size);
+                // ui.put(widget_rect, toggle2(&mut true));
+            });
+
+        // let tmp = egui::Vec2::new(400., 100.);
+        // //_frame.set_window_size(ctx.used_size());
+        // _frame.set_window_size(tmp);
     }
 }
 
@@ -106,12 +344,11 @@ pub fn toggle_ui2(ui: &mut egui::Ui, on: &mut bool) -> egui::Response {
         let rect = rect.expand(visuals.expansion);
         let radius = 0.43 * rect.height();
 
-         
-        let x_center = 200.;
-        let y_center = 140.;
+        let x_center = 120.;
+        let y_center = 150.;
         let center = egui::pos2(x_center, y_center);
         let radius_center = 5.32;
-    
+
         // outer rings
         ui.painter().circle(
             center,
@@ -125,33 +362,46 @@ pub fn toggle_ui2(ui: &mut egui::Ui, on: &mut bool) -> egui::Response {
             center,
             radius_center * 11.,
             egui::Color32::TRANSPARENT,
-            egui::Stroke::new(20.0, Color32::from_rgb(170, 70, 0)),
+            egui::Stroke::new(20.0, Color32::from_rgb(123, 167, 204)),
         );
-        ui.painter().circle(
-            center,
-            radius_center * 10.,
-            egui::Color32::TRANSPARENT,
-            egui::Stroke::new(2.0, Color32::from_rgb(170, 70, 0)),
-        );
-        ui.painter().circle(
-            center,
-            radius_center * 9.,
-            egui::Color32::TRANSPARENT,
-            egui::Stroke::new(2.0, Color32::from_rgb(170, 70, 0)),
-        );
+        // ui.painter().circle(
+        //     center,
+        //     radius_center * 10.,
+        //     egui::Color32::TRANSPARENT,
+        //     egui::Stroke::new(2.0, Color32::from_rgb(170, 70, 0)),
+        // );
+        // ui.painter().circle(
+        //     center,
+        //     radius_center * 9.,
+        //     egui::Color32::TRANSPARENT,
+        //     egui::Stroke::new(2.0, Color32::from_rgb(170, 70, 0)),
+        // );
 
-        // inner rings 
+        // inner rings
         ui.painter().circle(
             center,
             radius_center * 5.,
             egui::Color32::TRANSPARENT,
             egui::Stroke::new(10.0, Color32::from_rgb(123, 167, 204)),
         );
- 
     }
 
     response
 }
 pub fn toggle2(on: &mut bool) -> impl egui::Widget + '_ {
     move |ui: &mut egui::Ui| toggle_ui2(ui, on)
+}
+pub fn test() {
+    let options = eframe::NativeOptions {
+        initial_window_pos: Some(egui::Pos2::new(0.0, 00.0)),
+        resizable: true,
+        //initial_window_size: Some(egui::Vec2::new(880.0, 280.0)),
+        initial_window_size: Some(egui::Vec2::new(260.0, 260.0)),
+        decorated: false,
+        transparent: true,
+        drag_and_drop_support: true,
+        ..Default::default()
+    };
+
+    eframe::run_native(Box::new(super::controller::Application::default()), options);
 }
--- src/main.rs
+++ src/main.rs
@@ -7,46 +7,49 @@ mod finder;
 use futures::executor; // 0.3.1
 
 fn main() {
-    let sd = "/Users/matthew/tmp/file_types/audio/Short.mp3";
-    let filter = [true, true, true, true, true];
-    let single_file = return_dfer2(sd, filter);
-    println!("single_file => {:#?}", single_file);
+    // let sd = "/Users/matthew/tmp/file_types/audio/Short.mp3";
+    // let filter = [true, true, true, true, true];
+    // let single_file = return_dfer2(sd, filter);
+    // println!("single_file => {:#?}", single_file);
 
-    let sd = "/Users/matthew/tmp/file_types/audio";
-    let filter = [true, true, true, true, true];
-    let multi_batch = return_dfer2(sd, filter);
-    //println!("x => {:#?}", multi_batch);
+    // let sd = "/Users/matthew/tmp/file_types/audio";
+    // let filter = [true, true, true, true, true];
+    // let multi_batch = return_dfer2(sd, filter);
+    // //println!("x => {:#?}", multi_batch);
 
-    let single = single_file.data_set;
-    let multi = multi_batch.data_set;
+    // let single = single_file.data_set;
+    // let multi = multi_batch.data_set;
 
-    let mut matched = vec![];
-    // Loop through files to find
-    for collection in single {
-        let (key, _value) = collection;
+    // let mut matched = vec![];
+    // // Loop through files to find
+    // for collection in single {
+    //     let (key, _value) = collection;
 
-        // Loop through multi
-        for item in multi.iter() {
-            let (key2, _value2) = item;
+    //     // Loop through multi
+    //     for item in multi.iter() {
+    //         let (key2, _value2) = item;
 
-            if &key == key2 { 
-                matched.push(item);
-            }
-        }
-    }
+    //         if &key == key2 { 
+    //             matched.push(item);
+    //         }
+    //     }
+    // }
 
-    dbg!("{:#?}", matched);
+    // dbg!("{:#?}", matched);
 
-    let options = eframe::NativeOptions {
+    app::controller::test();
+  
+/*     let options = eframe::NativeOptions {
+        initial_window_pos: Some(egui::Pos2::new(300.0, 00.0)),
         resizable: true,
-        initial_window_size: Some(egui::Vec2::new(400.0, 280.0)),
+        initial_window_size: Some(egui::Vec2::new(880.0, 280.0)),
         decorated: true,
         transparent: true,
         drag_and_drop_support: true,
         ..Default::default()
     };
 
-    eframe::run_native(Box::new(app::controller::Application::default()), options);
+    eframe::run_native(Box::new(app::controller::Application::default()), options); */
 }
 
 fn return_dfer2(path: &str, filters: [bool; 5]) -> finder::finder::Finder {
