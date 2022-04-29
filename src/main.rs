15677c374da191806d6ca6f76127f6c2e09199b8
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-29 08:04:04 -0700

    sp

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,12 @@ fn main() {
     let path = PathBuf::from("./config.json");
     let config = config::get_config(&path);
 
-
+     
     //let _x = requests::get_repo_list::get_repo_list(&config, "sandbox".to_string());
+
+    let base64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=".to_string();
+    //let _x = requests::put_test::put_test(config.clone(), base64);
+
     //***************************************************** */
     // UI
     //***************************************************** */
